09f8bc70fc3a144a3f2ffafee9c34d00df8911e0
yuhe <dingyh94@yahoo.com>
2022-09-29 12:10:29 +0800

    feat: memo component

--- flame-graph-demo/src/FlameChart.js
+++ flame-graph-demo/src/FlameChart.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { FixedSizeList as List } from 'react-window'
 import { convertData } from './utils'
+
+const ListItem = memo(props => {
+  const { data, index, style } = props
+  const { ListData, width } = data
+  const nodes = ListData[index]
+  return (
+    <div style={style}>
+      {nodes.map((node, index) => (
+        <div
+          key={index}
+          className="Node"
+          style={{
+            left: node.offset * width,
+            width: node.width * width,
+            backgroundColor: node.color,
+          }}
+          title={node.name}
+        >
+          {node.name}
+        </div>
+      ))}
+    </div>
+  )
+})
 
 export default function FlameChart({ data, height, width }) {
   const ListData = convertData(data)
@@ -8,10 +32,12 @@ export default function FlameChart({ data, height, width }) {
     <List
       height={height}
       itemCount={ListData.length}
+      itemData={{ ListData, width }}
       itemSize={30}
       width={width}
     >
-      {({ index, style }) => {
+      {/* 每次父元素的渲染都会导致子元素的渲染，因此我们抽离出来作为一个pureComponent */}
+      {/* {({ index, style }) => {
         const nodes = ListData[index]
         return (
           <div style={style}>
@@ -31,7 +57,8 @@ export default function FlameChart({ data, height, width }) {
             ))}
           </div>
         )
-      }}
+      }} */}
+      {ListItem}
     </List>
   )
 }
